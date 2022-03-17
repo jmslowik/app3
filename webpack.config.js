e8f859b7767571b85b8570b90e1038d3f413a9af
jslowik <jakub.slowik@empik.com>
2022-03-17 02:45:29 +0100

    temp fix for production domain

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
+module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3003/",
+    publicPath: argv.mode === "production" ? "https://app3-jmslowik.vercel.app/" : "http://localhost:3003/",
   },
 
   resolve: {
@@ -63,4 +63,4 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
-};
+});
